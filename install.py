e4d989c66bd79869e6bc03d8443d8605e09c95fc
Kacper <przelozny14@gmail.com>
2024-03-29 22:03:37 +0100

    add readme and install script

---
+++ install.py
@@ -0,0 +1,7 @@
+import subprocess
+
+subprocess.run('pip install flask')
+subprocess.run('pip install bs4')
+subprocess.run('pip install xlwt')
+subprocess.run('pip install io')
+subprocess.run('pip install urllib')
